5e4a523c653fc7b9711b30ce3b816613497a7add
HabnerCBgabriel <145514514+HabnergabrielCB@users.noreply.github.com>
2025-08-27 21:49:51 -0300

    Update and rename Bolo.h to Produto.h

--- Projeto_CRUD/Produto.h
+++ Projeto_CRUD/Produto.h
@@ -1,26 +1,26 @@
-#ifndef BOLO_H
-#define BOLO_H
+#ifndef PRODUTO_H
+#define PRODUTO_H
 #include <string>
 #include <sstream>
 #include <iomanip>
 #include "Data.h"
 using namespace std;
 
-class Bolo {
+class Produto {
 protected:
-    string sabor;
+    string nome;
     float preco;
     int quantidade;
     Data dataFabricacao;
 public:
-    Bolo(string s="", float p=0.0, int q=0, Data d=Data())
-        : sabor(s), preco(p), quantidade(q), dataFabricacao(d) {}
+    Produto(string n="", float p=0.0, int q=0, Data d=Data())
+        : nome(n), preco(p), quantidade(q), dataFabricacao(d) {}
 
-    virtual ~Bolo() {}
+    virtual ~Produto() {}
     virtual string getTipo() const = 0;
 
-    string getSabor() const { return sabor; }
-    void setSabor(string s) { sabor = s; }
+    string getNome() const { return nome; }
+    void setNome(string n) { nome = n; }
 
     float getPreco() const { return preco; }
     void setPreco(float p) { preco = p; }
@@ -32,14 +32,14 @@ public:
         ostringstream oss;
         oss << fixed << setprecision(2) << preco;
 
-        return "[" + getTipo() + "] " + sabor + 
+        return "[" + getTipo() + "] " + nome + 
                " | R$ " + oss.str() +
                " | Qtd: " + to_string(quantidade) +
-               " | Data de entrega: " + dataFabricacao.toString();
+               " | Fabricação: " + dataFabricacao.toString();
     }
 
     virtual string serialize() const {
-        return getTipo() + ";" + sabor + ";" + to_string(preco) + ";" + 
+        return getTipo() + ";" + nome + ";" + to_string(preco) + ";" + 
                to_string(quantidade) + ";" + 
                to_string(dataFabricacao.dia) + ";" + 
                to_string(dataFabricacao.mes) + ";" + 
@@ -48,4 +48,3 @@ public:
 };
 
 #endif
-
