a56309a52b0f246e4da64bd25a6a5d31525770b6
felipebernardo10 <felipebernardo895@gmail.com>
2025-08-27 22:43:18 -0300

    Add files via upload

---
+++ Projeto_CRUD/Projeto_CRUD/BoloSimples.h
@@ -0,0 +1,13 @@
+#ifndef BOLOSIMPLES_H
+#define BOLOSIMPLES_H
+#include "Produto.h"
+
+class BoloSimples : public Produto {
+    public:
+    BoloSimples(string s="", float p=0.0, int q=0, Data d=Data())
+        : Produto(s,p,q,d) {}
+
+    string getTipo() const override { return "BoloSimples"; }
+};
+
+#endif
